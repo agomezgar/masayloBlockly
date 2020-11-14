c8f60103cb61f0a7ff5bfe4602639f0706832cee
Antonio Gómez <agomezgar@gmail.com>
2020-11-14 12:47:01 +0100

    Update Configuration.h

--- compilation/arduino/libraries/escornabot/Configuration.h
+++ compilation/arduino/libraries/escornabot/Configuration.h
@@ -106,13 +106,11 @@ See LICENSE.txt for details
 #define STEPPERS_MOTOR_LEFT_IN3 7
 #define STEPPERS_MOTOR_LEFT_IN4 6
 
-// step calibration (los comentados son los originales)
-//#define STEPPERS_STEPS_PER_SECOND 1000
-//#define STEPPERS_LINE_STEPS 1738
-//#define STEPPERS_TURN_STEPS 1024
-#define STEPPERS_STEPS_PER_SECOND 700
-#define STEPPERS_LINE_STEPS 850
-#define STEPPERS_TURN_STEPS 300
+// step calibration 
+#define STEPPERS_STEPS_PER_SECOND 1000
+#define STEPPERS_LINE_STEPS 1738
+#define STEPPERS_TURN_STEPS 1024
+
 #endif
 
 
