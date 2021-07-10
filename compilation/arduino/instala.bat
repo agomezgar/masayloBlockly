a9035329a126f9614b0f5437fd81cf4687b32438
Antonio Gómez <agomezgar@gmail.com>
2021-07-10 14:33:40 +0200

    De momento sólo arduinos

--- compilation/arduino/instala.bat
+++ compilation/arduino/instala.bat
@@ -1,2 +1,2 @@
 @echo off
-arduino-cli config init --overwrite --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json &&arduino-cli core update-index &&arduino-cli core install arduino:avr&&arduino-cli core install esp32:esp32&&arduino-cli core install esp8266:esp8266
+arduino-cli config init --overwrite --additional-urls https://arduino.esp8266.com/stable/package_esp8266com_index.json,https://dl.espressif.com/dl/package_esp32_index.json &&arduino-cli core update-index &&arduino-cli core install arduino:avr
